f1d4cf6739c71dbd92b069c693d32425ad1a325d
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-07 21:06:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+  five = 5
+  three = 3
+  if number % five == 0
+    return "fizz"
+  elseif number % three == 0
+    return "buzz"
+  elseif number % five == 0 & number % three ==0
+    return "fizzbuzz"
+  else 
+    return nill 
+  end 
+
+  
